4597392e028186d67cefb1845c1bf4c3fd47daa8
Tolgahan DEMİRBAŞ <todehan@gmail.com>
2018-12-24 17:24:40 +0300

    updated

--- routes/movie.js
+++ routes/movie.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+//Models
+const Movie = require('../models/Movie.js');
 
 router.post('/', (req, res, next) => {
-    const data = req.body;
-    res.json(data);
+    const {title, imdb_score, category, country, year} = req.body;
+
+    const movie = new Movie({
+        title: title,
+        imdb_score: imdb_score,
+        category: category,
+        country: country,
+        year: year
+    });
+
+    movie.save((err, data) => {
+        if(err)
+            res.json(err);
+        res.json(data);
+
+    });
+
 });
 
 module.exports = router;
