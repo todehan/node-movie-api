b0a89010c1f43f357483a507aa99b13800261298
Tolgahan DEMİRBAŞ <todehan@gmail.com>
2018-12-24 14:59:20 +0300

    updated

--- app.js
+++ app.js
@@ -14,3 +14,32 @@ const db = require('./helper/db.js')();
 //view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('users', users);
+
+//catch 404
+app.use((req, res, next) =>{
+   const err = new Error('Not Found');
+   err.status = 404;
+   next(err);
+});
+
+//error handler
+app.use((err, req, res, next) => {
+   //set locals
+   res.locals.message = err.message;
+   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+   //render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
